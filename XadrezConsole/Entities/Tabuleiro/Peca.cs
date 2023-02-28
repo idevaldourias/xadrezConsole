657a1255c7dd6e0499cfe26ce2213e9cb9570bf6
Idevaldo Urias <idevaldorodrigues@gmail.com>
2023-02-28 15:42:31 -0300

    Início da implementação do Tabuleiro

---
+++ XadrezConsole/Entities/Tabuleiro/Peca.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XadrezConsole.Entities.Tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+
+        public Cor Cor { get; set; }
+
+        public int QtdMovimentos { get; protected set; }
+
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tabuleiro = tabuleiro;
+            QtdMovimentos = 0;
+        }
+    }
+}
--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -6,10 +6,7 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            Posicao p;
-            p = new Posicao(3, 4);
-
-            Console.WriteLine("Posição: " + p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
         }
     }
 }
